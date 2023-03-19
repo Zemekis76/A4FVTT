636a0b10aa1b6d05a6b85ffe58bbc3a5a14d1a57
Zemekis76 <47531155+Zemekis76@users.noreply.github.com>
2023-03-19 20:42:35 +0100

    Update actor.js

--- module/actor.js
+++ module/actor.js
@@ -3,4 +3,15 @@ export class MySystemActor extends Actor {
         super.prepareData();
         this.data.items = this.data.items || [];
     }
+
+    async _onDrop(event) {
+        event.preventDefault();
+        const data = JSON.parse(event.dataTransfer.getData('text/plain'));
+        if (data.type === "Item") {
+            const item = await Item.fromDropData(data);
+            if (item) {
+                this.createEmbeddedDocuments("Item", [item.data]);
+            }
+        }
+    }
 }
